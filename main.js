6db143a328d0b00d71ad7567f631908166b5fa36
FlyingColors02 <shraduborse97@gmail.com>
2020-02-06 11:14:32 +0530

    #shraddha borse: used math.pow and ** operator to find 2 power of 10

---
+++ main.js
@@ -0,0 +1,10 @@
+//MAth.pow() function
+
+let numValue1=10;
+let result = Math.pow(numValue1,2);
+console.log(result);//100
+
+//   ** operator (double multiplication operator)
+
+result = numValue1**2;
+console.log(result);//100
